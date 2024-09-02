dd94f5084dbeed6620f1fd6222fb1b039eb1da3a
Ameya S <ameyashetty17@gmail.com>
2024-09-02 12:06:54 -0500

    .

--- src/Ginput.jsx
+++ src/Ginput.jsx
@@ -142,7 +142,7 @@ function Ginput({ setSubmitFlag, setFirstGame, setSecondGame, setShowNextCompone
           handleType(event.target.value, setGame2, setSearchResult2, setValidGame2)
         }
         fullWidth
-        placeholder="Enter a video game (e.g., Elden , Bloodborne)"
+        placeholder="Enter a video game (e.g., Sekiro , Bloodborne)"
         margin="normal"
       />
       {game2.length > 2 && searchResult2.length > 0 && (
